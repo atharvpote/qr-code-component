1b71ad18c9a5fb6c8cf5381799e008008f9abb69
Atharv Pote <atharvpote@gmail.com>
2023-06-20 21:52:58 +0530

    Build QR Code component

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontSize: {
+        title: "1.4rem",
+      },
+      textColor: {
+        grayishBlue: "#7B879D",
+      },
+      backgroundColor: {
+        lightGray: "#D6E2F0",
       },
     },
   },
